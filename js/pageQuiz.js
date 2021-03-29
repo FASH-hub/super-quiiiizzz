883d3bfc010aaba56947224f965625bed1705767
53854 <53854@etu.he2b.be>
2021-03-29 12:28:06 +0200

    Étape 1.3: choix du quiz

--- js/pageQuiz.js
+++ js/pageQuiz.js
@@ -24,20 +24,32 @@ function playQuiz(array) {
             break;
         case "jeux":
             tab = questions_jeux;
+            break;
         case "webg2":
             tab = questions_webg2;
+            break;
     }
     for (let i = 0; i < tab.length; i++) {
         $("#questions").append($("<div>").attr("id", tab[i].id))
         $("#" + tab[i].id).append($("<p>").text(tab[i].question));
 
-        for (let j = 0; j < tab[i].reponses.length; j++) {
-            $("#" + tab[i].id).append($("<input[name=answer]:checked>").text(tab[i].reponses[j]))
+        $("#" + tab[i].id).append($("<img>").attr("src", "../images/" + tab[i].image));
 
+        $("#" + tab[i].id).append($("<form>").attr("id", "answer-" + tab[i].id));
+        for (let j = 0; j < tab[i].reponses.length; j++) {
+
+            if (tab === questions_jeux || tab === questions_couples) {
+                $("#answer-" + tab[i].id).append($("<input type='checkbox' name='answer'>").text(tab[i].reponses[j]))
+
+            } else {
+                $("#answer-" + tab[i].id).append($("<input type='radio' name='answer'>").text(tab[i].reponses[j]))
+            }
+            $("#answer-" + tab[i].id).append($("<label>").text(tab[i].reponses[j]))
+            $("#answer-" + tab[i].id).append($("<br/>"));
         }
 
-        $("#" + tab[i].id).append($("<img>").attr("src", "../images/" + tab[i].image));
     }
+    $("#questions").append($("<button id='button' onclick='resultat()'>").text('soumettre'));
 }
 
 window.onload = displaySelectedQuiz();
